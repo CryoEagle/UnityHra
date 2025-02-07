71bfc51617bc7ea6f8caf46fc5a7cd83481a26eb
CryoEagle <43248056+CryoEagle@users.noreply.github.com>
2025-02-07 09:44:56 +0100

    Update

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    float speed = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        gameObject.transform.position += new Vector3(x, 0) * speed * Time.deltaTime;
+    }
+}
